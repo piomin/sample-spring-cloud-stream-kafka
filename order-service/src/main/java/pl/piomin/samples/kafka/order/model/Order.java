f884dda374e1f15d68bec0374dbf7926a38a7447
piomin <piotr.minkowski@gmail.com>
2026-03-08 15:21:21 +0100

    update to latest spring cloud

--- order-service/src/main/java/pl/piomin/samples/kafka/order/model/Order.java
+++ order-service/src/main/java/pl/piomin/samples/kafka/order/model/Order.java
@@ -4,15 +4,10 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Getter
-@Setter
-@ToString
-@AllArgsConstructor
-@NoArgsConstructor
 public class Order {
     private Long id;
     private Integer customerId;
@@ -23,4 +18,105 @@ public class Order {
     private LocalDateTime creationDate;
     private OrderType type;
     private int amount;
+
+    public Order() {
+    }
+
+    public Order(Long id, Integer customerId, Integer productId, int productCount, LocalDateTime creationDate, OrderType type, int amount) {
+        this.id = id;
+        this.customerId = customerId;
+        this.productId = productId;
+        this.productCount = productCount;
+        this.creationDate = creationDate;
+        this.type = type;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+    public int getProductCount() {
+        return productCount;
+    }
+
+    public void setProductCount(int productCount) {
+        this.productCount = productCount;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public OrderType getType() {
+        return type;
+    }
+
+    public void setType(OrderType type) {
+        this.type = type;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "id=" + id +
+                ", customerId=" + customerId +
+                ", productId=" + productId +
+                ", productCount=" + productCount +
+                ", creationDate=" + creationDate +
+                ", type=" + type +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Order order = (Order) o;
+        return productCount == order.productCount &&
+                amount == order.amount &&
+                Objects.equals(id, order.id) &&
+                Objects.equals(customerId, order.customerId) &&
+                Objects.equals(productId, order.productId) &&
+                Objects.equals(creationDate, order.creationDate) &&
+                type == order.type;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, customerId, productId, productCount, creationDate, type, amount);
+    }
 }
